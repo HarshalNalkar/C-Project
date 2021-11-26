3c6c390a3d4c7be5e9344e033520928863e7bae7
Harshal Nalkar <95092091+HarshalNalkar@users.noreply.github.com>
2021-11-27 00:45:37 +0530

    Update Why the output coming 25 not 28.c

--- Why the output coming 25 not 28.c
+++ Why the output coming 25 not 28.c
@@ -6,6 +6,8 @@ int main()
     printf ("Temperature in Fahrenheit is %.2f", (3/4)*c + 25);
   
     //beacuse division contain the intger value, it do not contain foalt value.
-  
+    // (3/4)*4.0+25 --> (int/int)*float+int
+    //   0*4.0+25   -->   int*float+int
+    //   25         -->    int   
   return 0;
 }
