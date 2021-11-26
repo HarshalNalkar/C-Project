8bb053f3ff48fb0100ef2d42f4973f01d9a8963e
Harshal Nalkar <95092091+HarshalNalkar@users.noreply.github.com>
2021-11-27 01:02:27 +0530

    Create add, sub, div, multi without using 3rd variable.c

---
+++ add, sub, div, multi without using 3rd variable.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int a, b;
+  printf("Enter the values a and b");
+  scanf("%d%d",&a,&b);
+ 
+  printf("Additional value is %d\n", a+b);
+  printf("Substraction value is %d\n", a-b);
+  printf("Division valus is %d\n", a/b);
+  printf("Multiplication value is %d\n", a*b);
+  
+  return 0;
+}  
+  
